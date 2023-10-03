3428d9e9dcbfb5ba2286bcd07ea9411fe7435058
Alyson Gadelha <alyson_sud@hotmail.com>
2023-10-03 22:26:55 +0100

    BLOG-0003: add new responsive break to carrousel

--- section/FeaturedPosts.tsx
+++ section/FeaturedPosts.tsx
@@ -25,6 +25,10 @@ const FeaturedPosts = (props: Props) => {
       breakpoint: { max: 4000, min: 1024 },
       items: 5,
     },
+    largeDesktop: {
+      breakpoint: { max: 2500, min: 1024 },
+      items: 4,
+    },
     desktop: {
       breakpoint: { max: 1024, min: 768 },
       items: 3,
